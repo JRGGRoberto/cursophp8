3340cfde2ad8d48d6220a60942be239e8443ef9a
Roberto <jrggroberto@gmail.com>
2024-04-29 08:21:01 -0300

    get from github

--- www/blog/index.php
+++ www/blog/index.php
@@ -10,22 +10,5 @@ include_once 'sistema/helpers.php';
 
 echo saudacao();
 echo '<hr>';
-<<<<<<< HEAD
-echo formatarValor(21231231);
-echo '<hr>';
-echo formatarNumero(1111111);
-
-echo '<hr>';
-$data = date('d/m/Y H:i:s');
-
-
-echo contarTempo('2024-04-26 16:38:05');
-echo '<hr>';
-*/
-var_dump( validarEmail('j@com.br'));
-
-echo '<hr><a href="../" class="btn btn-primary btn-sm">Voltar</a>';
-=======
 $cpf = '004.348.491-38';
 echo 'O CPF '.$cpf. ' é ' . ( validaCPF($cpf) ? 'Válido' : 'inválido;');
->>>>>>> 25b569af38e13a5a9e21b1b334d37ce460e4e06c
