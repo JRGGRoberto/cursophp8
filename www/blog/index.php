5fa19a4a1d9dfe62afa77f3fb35a4e9be7ff8516
Roberto <jrggroberto@gmail.com>
2024-05-01 06:56:29 -0300

    feat:49 created composer.json file. related to #4

--- www/blog/index.php
+++ www/blog/index.php
@@ -4,7 +4,9 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 */
+require 'vendor/autoload.php';
 require_once './sistema/configuracao.php';
-include './sistema/Entity/Helpers.php';
+use sistema\Entity\Helpers;
 
-echo sistema\Entity\Helpers::dataAtual();
+echo SITE_DESCRICAO;
+echo Helpers::saudacao() .', '. Helpers::dataAtual() ;
---
+++ www/blog/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'jrggroberto/cursophp8',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '87b33f8a398c2ef9d825de8ada427d934a191cd3',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jrggroberto/cursophp8' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '87b33f8a398c2ef9d825de8ada427d934a191cd3',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
