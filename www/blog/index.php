87b33f8a398c2ef9d825de8ada427d934a191cd3
Roberto <jrggroberto@gmail.com>
2024-05-01 06:38:55 -0300

    maneira de chamar a class sem usar o use

--- www/blog/index.php
+++ www/blog/index.php
@@ -5,13 +5,6 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 */
 require_once './sistema/configuracao.php';
-include './sistema/Entity/Mensagem.php';
-include './sistema/Entity/Controller.php';
 include './sistema/Entity/Helpers.php';
 
-use \sistema\Entity\Controller;
-$controler = new Controller();
-var_dump($controler);
-
-use \sistema\Entity\Helpers;
-echo Helpers::dataAtual();
+echo sistema\Entity\Helpers::dataAtual();
